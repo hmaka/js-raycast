80c8d9d8c6835e5d744b79f7271f7ffab9bec16b
Havish <Havish747@gmail.com>
2023-05-09 16:01:39 -0400

    Finished project!

--- index.js
+++ index.js
@@ -14,6 +14,10 @@ const PLAYER_SIZE = 10;
 const FOV = toRadians(60);
 
 const COLORS = {
+  floor: "purple", // "#ff6361"
+  ceiling: "#ffffff", // "#012975",
+  wall: "#013aa6", // "#58508d"
+  wallDark: "#012975", // "#003f5c"
   rays: "#ffa600"
 }
 
@@ -52,13 +56,120 @@ function outOfMapBounds(x, y) {
 function distance(x1, y1, x2, y2) {
   return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
 }
+function getVCollision(angle) {
+  const right = Math.abs(Math.floor((angle - Math.PI / 2) / Math.PI) % 2);
+
+  const firstX = right
+    ? Math.floor(player.x / CELL_SIZE) * CELL_SIZE + CELL_SIZE
+    : Math.floor(player.x / CELL_SIZE) * CELL_SIZE;
+
+  const firstY = player.y + (firstX - player.x) * Math.tan(angle);
+
+  const xA = right ? CELL_SIZE : -CELL_SIZE;
+  const yA = xA * Math.tan(angle);
+
+  let wall;
+  let nextX = firstX;
+  let nextY = firstY;
+  while (!wall) {
+    const cellX = right
+      ? Math.floor(nextX / CELL_SIZE)
+      : Math.floor(nextX / CELL_SIZE) - 1;
+    const cellY = Math.floor(nextY / CELL_SIZE);
+
+    if (outOfMapBounds(cellX, cellY)) {
+      break;
+    }
+    wall = map[cellY][cellX];
+    if (!wall) {
+      nextX += xA;
+      nextY += yA;
+    } else {
+    }
+  }
+  return {
+    angle,
+    distance: distance(player.x, player.y, nextX, nextY),
+    vertical: true,
+  };
+}
+
+function getHCollision(angle) {
+  const up = Math.abs(Math.floor(angle / Math.PI) % 2);
+  const firstY = up
+    ? Math.floor(player.y / CELL_SIZE) * CELL_SIZE
+    : Math.floor(player.y / CELL_SIZE) * CELL_SIZE + CELL_SIZE;
+  const firstX = player.x + (firstY - player.y) / Math.tan(angle);
+
+  const yA = up ? -CELL_SIZE : CELL_SIZE;
+  const xA = yA / Math.tan(angle);
+
+  let wall;
+  let nextX = firstX;
+  let nextY = firstY;
+  while (!wall) {
+    const cellX = Math.floor(nextX / CELL_SIZE);
+    const cellY = up
+      ? Math.floor(nextY / CELL_SIZE) - 1
+      : Math.floor(nextY / CELL_SIZE);
+
+    if (outOfMapBounds(cellX, cellY)) {
+      break;
+    }
+
+    wall = map[cellY][cellX];
+    if (!wall) {
+      nextX += xA;
+      nextY += yA;
+    }
+  }
+  return {
+    angle,
+    distance: distance(player.x, player.y, nextX, nextY),
+    vertical: false,
+  };
+}
+
+function castRay(angle) {
+  const vCollision = getVCollision(angle);
+  const hCollision = getHCollision(angle);
+
+  return hCollision.distance >= vCollision.distance ? vCollision : hCollision
+}
+
+function fixFishEye(distance, angle, playerAngle) {
+  const diff = angle - playerAngle;
+  return distance * Math.cos(diff);
+}
 
 function getRays() {
-  return []
+  const intialAngle = player.angle - FOV / 2;
+  const numberOfRays = SCREEN_WIDTH;
+  const angleStep = FOV / numberOfRays;
+
+  return Array.from({ length: numberOfRays }, (_, i) => {
+    const angle = intialAngle + i * angleStep;
+    const ray = castRay(angle);
+    return ray
+  })
 }
 
 function renderScene(rays) {
-
+  rays.forEach((ray, i) => {
+    const distance = fixFishEye(ray.distance, ray.angle, player.angle);
+    const wallHeight = ((CELL_SIZE * 5) / distance) * 277;
+    context.fillStyle = ray.vertical ? COLORS.wallDark : COLORS.wall;
+    context.fillRect(i, SCREEN_HEIGHT / 2 - wallHeight / 2, 1, wallHeight);
+    context.fillStyle = COLORS.floor;
+    context.fillRect(
+      i,
+      SCREEN_HEIGHT / 2 + wallHeight / 2,
+      1,
+      SCREEN_HEIGHT / 2 - wallHeight / 2
+    );
+    context.fillStyle = COLORS.ceiling;
+    context.fillRect(i, 0, 1, SCREEN_HEIGHT / 2 - wallHeight / 2);
+  });
 }
 
 function renderMiniMap(posX = 0, posY = 0, scale = 1, rays) {
@@ -92,7 +203,16 @@ function renderMiniMap(posX = 0, posY = 0, scale = 1, rays) {
   context.stroke()
 
   context.strokeStyle = COLORS.rays;
-
+  rays.forEach((ray) => {
+    context.beginPath();
+    context.moveTo(player.x * scale, player.y * scale);
+    context.lineTo(
+      (player.x + Math.cos(ray.angle) * ray.distance) * scale,
+      (player.y + Math.sin(ray.angle) * ray.distance) * scale
+    );
+    context.closePath();
+    context.stroke();
+  });
 }
 
 function gameLoop() {
