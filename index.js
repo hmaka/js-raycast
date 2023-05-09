21c8a91974ca4ef6f61107649b86a0c40097266e
Havish <Havish747@gmail.com>
2023-05-08 23:40:36 -0400

    Further progress

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const SCREEN_WIDTH = 1024;
-const SCREEN_HEIGHT = 768;
+const SCREEN_WIDTH = 900;
+const SCREEN_HEIGHT = 675;
 
 const canvas = document.createElement("canvas")
 canvas.setAttribute("width", SCREEN_WIDTH)
@@ -11,6 +11,11 @@ const TICK = 30;
 
 const CELL_SIZE = 64;
 const PLAYER_SIZE = 10;
+const FOV = toRadians(60);
+
+const COLORS = {
+  rays: "#ffa600"
+}
 
 const map = [
   [1, 1, 1, 1, 1, 1, 1],
@@ -36,7 +41,16 @@ function clearScreen() {
 }
 
 function movePlayer() {
+  player.x += Math.cos(player.angle) * player.speed;
+  player.y += Math.sin(player.angle) * player.speed;
 
+}
+
+function outOfMapBounds(x, y) {
+  return x < 0 || x >= map[0].length || y < 0 || y >= map.length;
+}
+function distance(x1, y1, x2, y2) {
+  return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
 }
 
 function getRays() {
@@ -61,11 +75,24 @@ function renderMiniMap(posX = 0, posY = 0, scale = 1, rays) {
 
   context.fillStyle = "red";
   context.fillRect(
-    posX + player.x * scale - PLAYER_SIZE,
-    posY + player.y * scale - PLAYER_SIZE * 2,
+    posX + player.x * scale - PLAYER_SIZE / 2,
+    posY + player.y * scale - PLAYER_SIZE / 2,
     PLAYER_SIZE,
     PLAYER_SIZE
   )
+  const rayLength = PLAYER_SIZE * 2;
+  context.strokeStyle = "red"
+  context.beginPath()
+  context.moveTo(player.x * scale + posX, player.y * scale + posY)
+  context.lineTo(
+    (player.x + Math.cos(player.angle) * rayLength) * scale,
+    (player.y + Math.sin(player.angle) * rayLength) * scale,
+  )
+  context.closePath()
+  context.stroke()
+
+  context.strokeStyle = COLORS.rays;
+
 }
 
 function gameLoop() {
@@ -78,7 +105,9 @@ function gameLoop() {
 
 setInterval(gameLoop, TICK)
 
-
+function toRadians(degrees) {
+  return (degrees * Math.PI) / 180;
+}
 
 document.addEventListener("keydown", (e) => {
   if (e.key === "w") {
@@ -86,11 +115,20 @@ document.addEventListener("keydown", (e) => {
   }
 })
 
-document.addEventListener("keyup", (e) => {
+document.addEventListener("keydown", (e) => {
   if (e.key === "s") {
     player.speed = -2
   }
 })
 
+document.addEventListener("keyup", (e) => {
+  if (e.key === "s" || e.key === "w") {
+    player.speed = 0
+  }
+})
+
+document.addEventListener("mousemove", (e) => {
+  player.angle += toRadians(e.movementX)
+})
 
 
